09f698994b3d541bd72bbe30c42bebad917f0ed4
cholliday98 <hollidaylcade@gmail.com>
2015-04-24 18:26:09 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
 var post;var upvote;var gold;var items;var upgrades;
-var pstcnt = document.getElementById('pst');
+var pstcnt = document.getElementById('pos');
 var golcnt = document.getElementById('gol');
 var upvcnt = document.getElementById('upv');
 var pstbtn = document.getElementById('button_holder');
 var upvbtn = document.getElementById('upvotebutton_holder');
 var golbtn = document.getElementById('goldbutton_holder');
-post = [0, 1, 1, 3500];
-upvote = [0, 1, 1, 1500, 0];
-gold = [0, 1, 1, 5000, 0];
+post = [0, 1, 1, 1250];
+upvote = [0, 1, 1, 500, 0];
+gold = [0, 1, 1, 2500, 0];
+store = [0, 0, 0, 0, 0];
 
 function initializing() { 
 	console.log("Initializing()");
@@ -20,17 +21,31 @@ function unlock(){
 	}
 }
 
+function buy(num){
+	if(num == 1){
+		var cost = Math.ceil(store[0]+(store[0]*store[0])+15);
+		if(upvote[0] >= cost){
+			store[0] += 1;
+			upvote[0] -= cost;
+			document.getElementById('1').innerHTML = store[0];
+			upvcnt.innerHTML= upvote[0];
+		}else{
+			console.log("Not enough upvotes");
+		}
+	}
+}
+
 var f= [0, 0, 0];
 function failurecount(type){
 	if (type == 'pos') {;
 		f[0]+= 1;
-		console.log("This isn't Tumblr slow the fuck down.  I've calmed you " + f[0] + " times, already");
+		console.log("Post Overload #" + f[0]);
 	}else if(type == 'upv'){
 		f[1]+= 1;
-		console.log("You can't love everything.  I've depressed you " + f[1] + " times, already");
+		console.log("Upvote Overload #" + f[1]);
 	}else if (type == 'gol'){
 		f[2]+= 1;
-		console.log("I've only got one pickaxe.  I've repremanded you " + f[2] +" times, already");
+		console.log("Gold Overload #" + f[2]);
 	}
 }
 
@@ -69,10 +84,6 @@ function actionclick(type){
 			golcnt.innerHTML= gold[0];
 		},gold[3]);
 	}
-}
-
-function interval(time){
-	
 }
 
 window.onload = function(){
